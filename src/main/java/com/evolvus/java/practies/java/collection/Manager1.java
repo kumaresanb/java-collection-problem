8f7d70212fc307d4ccfb80e875919480343204d2
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2018-12-05 11:10:15 +0530

    arrays

---
+++ src/main/java/com/evolvus/java/practies/java/collection/Manager1.java
@@ -0,0 +1,16 @@
+package com.evolvus.java.practies.java.collection;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class Manager1 
+{
+    public static void main( String[] args )
+    {
+       int z[][]= {{1,2},{23,45}};
+       System.out.println(Arrays.toString(new int[] {12,34,45}));
+    }
+}
---
+++ src/main/java/com/evolvus/java/practies/java/collection/Person.java
@@ -0,0 +1,35 @@
+package com.evolvus.java.practies.java.collection;
+
+import java.util.Arrays;
+
+public class Person implements Comparable {
+	
+	String name;
+	int age;
+	double weight;
+	public Person(String name,int age,double weight) {
+		this.name=name;
+		this.age=age;
+		this.weight=weight;
+	}
+		
+	public int compareTo(Object obj) {
+		Person p=(Person) obj;
+		return this.age-p.age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Name="+name+" Age="+age+" Weigh="+weight;
+	}
+	
+	public static void main(String[] args) {
+		Person p1=new Person("kumar", 32, 81.6);
+		Person p2=new Person("ravi", 28, 75.00);
+		Person p3=new Person("ram", 22, 70.00);
+		Person[] all= {p1,p2,p3};
+		System.out.println(Arrays.toString(all));
+		Arrays.sort(all);
+		System.out.println(Arrays.toString(all));
+	}
+}
